644c4064428c74ab23ff5f70a7cc550564e217c4
Thareek Anvar <thareekanvar@gmail.com>
2019-09-26 11:14:29 +0000

    ImagManipulator function Problem Fixed in Upload Section

--- utils/shrinkImageAsync.js
+++ utils/shrinkImageAsync.js
@@ -1,7 +1,7 @@
 import * as ImageManipulator from 'expo-image-manipulator';
 
 function reduceImageAsync(uri) {
-  return ImageManipulator.manipulate(uri, [{ resize: { width: 500 } }], {
+  return ImageManipulator.manipulateAsync(uri, [{ resize: { width: 500 } }], {
     compress: 0.5,
   });
 }
